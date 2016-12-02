45f68af569ddab1dbee80a5375cc2f2dd2431164
coonstiffany <coons.tiffany@gmail.com>
2016-12-02 01:08:06 -0500

    T commit?

---
+++ oTree/models.py
@@ -0,0 +1,65 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+import csv
+
+author = 'Your name here'
+
+doc = """
+A quiz app that reads its questions from a spreadsheet
+(see triangle.csv in this directory).
+There is 1 set of payoffs withing the triangle per page;
+the number of pages in the game
+is determined by the number of payoff sets in the CSV.
+See the comment below about how to randomize the order of pages.
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'Machina Triangle'
+    players_per_group = None
+
+    with open('machina_triangle/machina_triangle.csv') as f:
+        payoff_set = list(csv.DictReader(f))
+
+    num_triangles = len(payoff_set)
+
+
+class Subsession(BaseSubsession):
+    def before_session_starts(self):
+        if self.round_number == 1:
+            self.session.vars['questions'] = Constants.questions
+            ## ALTERNATIVE DESIGN:
+            ## to randomize the order of the questions, you could instead do:
+
+            # import random
+            # randomized_questions = random.sample(Constants.questions, len(Constants.questions))
+            # self.session.vars['questions'] = randomized_questions
+
+            ## and to randomize differently for each participant, you could use
+            ## the random.sample technique, but assign into participant.vars
+            ## instead of session.vars.
+
+        for p in self.get_players():
+            question_data = p.current_question()
+            p.question_id = question_data['id']
+            p.question = question_data['payoff_set']
+            #p.triangle = get_triangle(question_data)
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    question_id = models.PositiveIntegerField()
+    question = models.CharField()
+    submitted_answer = models.CharField(widget=widgets.RadioSelect())
+    #is_correct = models.BooleanField()
+
+    def current_question(self):
+        return self.session.vars['questions'][self.round_number - 1]
+
+    def check_correct(self):
+        self.preference = self.submitted_answer
